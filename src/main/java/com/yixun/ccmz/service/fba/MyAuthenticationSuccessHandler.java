dc7b481cd78e809ff3c57bf6c09a409c8329f36e
fxwdl <fx__wdl@hotmail.com>
2018-07-23 17:01:28 +0800

    又改了一下登录认证，恢复默认配置。总是跳到首页

--- src/main/java/com/yixun/ccmz/service/fba/MyAuthenticationSuccessHandler.java
+++ src/main/java/com/yixun/ccmz/service/fba/MyAuthenticationSuccessHandler.java
@@ -15,13 +15,17 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.WebAttributes;
+import org.springframework.security.web.authentication.AbstractAuthenticationTargetUrlRequestHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
-public class MyAuthenticationSuccessHandler implements AuthenticationSuccessHandler // extends
-																					// SavedRequestAwareAuthenticationSuccessHandler
+public class MyAuthenticationSuccessHandler extends AbstractAuthenticationTargetUrlRequestHandler
+		implements AuthenticationSuccessHandler
+// // extends
+// SavedRequestAwareAuthenticationSuccessHandler
 {
 
+	// 1.继承SavedRequestAwareAuthenticationSuccessHandler的实现
 	// @Override
 	// public void onAuthenticationSuccess(HttpServletRequest request,
 	// HttpServletResponse response,
@@ -36,57 +40,85 @@ public class MyAuthenticationSuccessHandler implements AuthenticationSuccessHand
 	// super.onAuthenticationSuccess(request, response, authentication);
 	// }
 
-	protected Log logger = LogFactory.getLog(this.getClass());
+	// 2.完全从0实现AuthenticationSuccessHandler接口
 
-	private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+	// protected Log logger = LogFactory.getLog(this.getClass());
+	//
+	// private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+	//
+	// public void onAuthenticationSuccess(HttpServletRequest request,
+	// HttpServletResponse response,
+	// Authentication authentication) throws IOException
+	// {
+	//
+	// handle(request, response, authentication);
+	// clearAuthenticationAttributes(request);
+	// }
+	//
+	// protected void handle(HttpServletRequest request, HttpServletResponse
+	// response, Authentication authentication)
+	// throws IOException
+	// {
+	//
+	// String targetUrl = determineTargetUrl(authentication);
+	//
+	// if (response.isCommitted())
+	// {
+	// logger.debug("Response has already been committed. Unable to redirect to " +
+	// targetUrl);
+	// return;
+	// }
+	//
+	// redirectStrategy.sendRedirect(request, response, targetUrl);
+	// }
+	//
+	// protected String determineTargetUrl(Authentication authentication)
+	// {
+	// return "/index";
+	// }
+	//
+	// protected void clearAuthenticationAttributes(HttpServletRequest request)
+	// {
+	// HttpSession session = request.getSession(false);
+	// if (session == null)
+	// {
+	// return;
+	// }
+	// session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+	// }
+	//
+	// public void setRedirectStrategy(RedirectStrategy redirectStrategy)
+	// {
+	// this.redirectStrategy = redirectStrategy;
+	// }
+	//
+	// protected RedirectStrategy getRedirectStrategy()
+	// {
+	// return redirectStrategy;
+	// }
 
-	@Override
+	// 3.继承AbstractAuthenticationTargetUrlRequestHandler
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-			Authentication authentication) throws IOException
+			Authentication authentication) throws IOException, ServletException
 	{
 
 		handle(request, response, authentication);
 		clearAuthenticationAttributes(request);
 	}
 
-	protected void handle(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
-			throws IOException
-	{
-
-		String targetUrl = determineTargetUrl(authentication);
-
-		if (response.isCommitted())
-		{
-			logger.debug("Response has already been committed. Unable to redirect to " + targetUrl);
-			return;
-		}
-
-		redirectStrategy.sendRedirect(request, response, targetUrl);
-	}
-
-	protected String determineTargetUrl(Authentication authentication)
-	{
-		return "/index";
-	}
-
-	protected void clearAuthenticationAttributes(HttpServletRequest request)
+	/**
+	 * Removes temporary authentication-related data which may have been stored in
+	 * the session during the authentication process.
+	 */
+	protected final void clearAuthenticationAttributes(HttpServletRequest request)
 	{
 		HttpSession session = request.getSession(false);
+
 		if (session == null)
 		{
 			return;
 		}
+
 		session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
 	}
-
-	public void setRedirectStrategy(RedirectStrategy redirectStrategy)
-	{
-		this.redirectStrategy = redirectStrategy;
-	}
-
-	protected RedirectStrategy getRedirectStrategy()
-	{
-		return redirectStrategy;
-	}
-
 }
